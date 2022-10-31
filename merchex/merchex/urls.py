425339ffad6b8e9915f7ba129529b22e511c5fc9
idoleke ganiou <ganiouidoleke7@gmail.com>
2022-10-31 09:17:21 +0100

    deployement sur github

---
+++ merchex/merchex/urls.py
@@ -0,0 +1,27 @@
+"""merchex URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from rhino import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.app_rhino, name='rhino-us'),
+    path('about/', views.app_about, name='about-us'),
+    path('contact/', views.app_contact,name='contact-us'),
+    path('project/', views.app_project, name='project-us'),
+    path('staff/', views.app_staff, name='staff-us'),
+]
---
+++ merchex/rhino/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RhinoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rhino'
